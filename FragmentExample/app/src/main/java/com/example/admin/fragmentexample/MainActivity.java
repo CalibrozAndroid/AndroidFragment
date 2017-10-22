4f65422f6701004404f1739592344299ea93c852
Muhammed Thasneem <muhammed.thasneem@yahoo.com>
2017-10-22 12:18:55 +0530

    final code

--- FragmentExample/app/src/main/java/com/example/admin/fragmentexample/MainActivity.java
+++ FragmentExample/app/src/main/java/com/example/admin/fragmentexample/MainActivity.java
@@ -1,13 +1,45 @@
 package com.example.admin.fragmentexample;
 
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.admin.fragmentexample.fragments.FragmentOne;
+import com.example.admin.fragmentexample.fragments.FragmentTwo;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button mFragmentOneButton;
+    private Button mFragmentTwoButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mFragmentOneButton = (Button) findViewById(R.id.frgamentOneButton);
+        mFragmentTwoButton = (Button) findViewById(R.id.fragmentTwoButton);
+
+
+        mFragmentOneButton.setOnClickListener(this);
+        mFragmentTwoButton.setOnClickListener(this);
+
+        onClick(mFragmentOneButton);
+    }
+
+    @Override
+    public void onClick(View v) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        switch (v.getId()) {
+            case R.id.frgamentOneButton:
+                transaction.replace(R.id.container,new FragmentOne(),"fragmentone").commit();
+                break;
+            case R.id.fragmentTwoButton:
+                transaction.replace(R.id.container,new FragmentTwo(),"fragmentone").commit();
+                break;
+        }
+
     }
 }
